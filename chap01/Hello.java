6516aecf2e8c8b1febe55dc04afa81db85dd3c9d
cys <dptmf2287@naver.com>
2024-03-18 12:27:35 +0900

    자바 실습

--- chap01/Hello.java
+++ chap01/Hello.java
@@ -1,10 +1,9 @@
-package sec06;
 
 public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("ddw");
+		System.out.println("abc");
 	}
 
 }
